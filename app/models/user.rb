058a07dcd702081342172d15def8122baf58b763
ryotaro3983 <110594547+ryotaro3983@users.noreply.github.com>
2022-10-18 18:19:44 +0900

    正規表現のバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :first_name,       presence: true
+    validates :last_name,        presence: true
+  end
   
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence:true
-  validates :last_name_kana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :first_name_kana,  presence:true
+    validates :last_name_kana,   presence: true
+  end
+  validates :nickname,         presence: true
+  validates :birthday,         presence: true
 end

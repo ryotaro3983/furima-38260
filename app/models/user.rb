858d77fbce8a9d4b6d7bf9f450ba00de4070c0df
ryotaro3983 <110594547+ryotaro3983@users.noreply.github.com>
2022-10-19 21:42:20 +0900

    メッセージ文修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX }, length: {maximum: 40}
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
-    validates :first_name,       presence: true
-    validates :last_name,        presence: true
-  end
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' }, length: {maximum: 40}
   
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
-    validates :first_name_kana,  presence:true
-    validates :last_name_kana,   presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters'} do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
   end
-  validates :nickname,         presence: true
-  validates :birthday,         presence: true
 end

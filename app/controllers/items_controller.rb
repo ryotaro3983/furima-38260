e1761d3760b53be1fa0fd21a6733bd5c75163742
ryotaro3983 <110594547+ryotaro3983@users.noreply.github.com>
2022-10-28 10:58:32 +0900

    保存可能にカラム名修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,9 +8,18 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
    def item_params
     params.require(:item).permit(:name, :text, :category_id, :condition_id, 
-      :delivery_fee_id, :prefecture_id, :delivery_day_id, :image).merge(user_id: current_user.id)
+      :delivery_fee_id, :prefecture_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
    end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   validates :name       ,presence: true
   validates :text       ,presence: true
+  validates :image      ,presence: true
   validates :price      ,presence: true, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   
   with_options numericality: { other_than: 1 , message: "can't be blank"} do

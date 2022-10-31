f3a249fbd5b7aa9fd39da012124d7bd77a2245e6
ryotaro3983 <110594547+ryotaro3983@users.noreply.github.com>
2022-10-31 11:08:32 +0900

    rubocop適用

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :text, presence: true
   validates :image, presence: true
-  
+
   validates :price, presence: true, numericality: { only_integer: true, message: 'is invalid. Input half-width characters' }
   validates :price, presence: true,
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }

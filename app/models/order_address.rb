97f292910ec2efcb1a4a11d9c6a1357d704fd6e2
ryotaro3983 <110594547+ryotaro3983@users.noreply.github.com>
2022-11-07 21:23:14 +0900

    addressデータ作成機能まで

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,19 +1,19 @@
-class DonationAddress
+class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture, :city, :house_number, :building, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number
 
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :city
     validates :house_number
-    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid."}
+    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Input only number"}
   end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
-    Address.create(create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building: building, order_id: order.id))
+    Address.create(create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, order_id: order.id))
   end
 end
